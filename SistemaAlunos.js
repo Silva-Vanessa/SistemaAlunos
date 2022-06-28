7bc27cc556ed5438b275d14a1af676024c8ff8df
Vanessa Silva <vanessaprogramacao@gmail.com>
2022-06-28 19:38:07 -0300

    Sistema alunos - par, impar, zero

---
+++ SistemaAlunos.js
@@ -0,0 +1,12 @@
+var numeroAlunos = 10
+
+for (var contador = 0; contar < numeroAlunos.length; contador++){
+
+    if(contador ==0){
+        console.log("O número atual é zero")
+    }else if(contador %2 == 0){
+        console.log("O número é par")
+    }else{
+        console.log ("O número é impar")
+    }
+}
